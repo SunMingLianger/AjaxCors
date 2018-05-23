85648c3e4b0a66f65e2fa997e6ff1821ee79b361
SunMIngLianger <283076358@qq.com>
2018-05-23 09:14:49 +0800

    jsonp的跨域时，后台做的相应改动

--- src/main/java/com/sml/ajaxserver/controller/JsonAdvice.java
+++ src/main/java/com/sml/ajaxserver/controller/JsonAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 /**
+ * jsonp的跨域请求，后台的相应配置
  * Created by 神迷的亮
  * 2018-05-22 15:06
  */
